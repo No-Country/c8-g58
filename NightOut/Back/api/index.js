99aa4cfc309d917698ff18df082c7def717cdf1d
FrancoMED <francoemedina@gmail.com>
2022-12-07 16:45:29 -0300

    var .env franco

--- NightOut/Back/api/index.js
+++ NightOut/Back/api/index.js
@@ -2,14 +2,14 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const { preload } = require('./src/controllers/publication/getPublication.js');
 const { Publication } = require('./src/db.js');
-
+const { PORT } = process.env || 3001;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(async () => {
 	const data = await Publication.findAll();
 	if (!data.length) {
 		await preload();
 	}
-	server.listen(3001, () => {
-		console.log('%s listening at 3001'); // eslint-disable-line no-console
+	server.listen(PORT, () => {
+		console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
 	});
 });
